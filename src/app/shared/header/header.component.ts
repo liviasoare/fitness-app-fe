d7f6b4371b3a2401bcef2b6fa32367122a1f2e00
Livia Soare <liv13soare@gmail.com>
2024-09-10 18:15:16 +0300

    added pink theme

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,72 +1,84 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    RippleModule,
+    CommonModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   items: MenuItem[] | undefined;
 
   ngOnInit() {
-      this.items = [
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
           {
-              label: 'Home',
-              icon: 'pi pi-home'
+            label: 'Core',
+            icon: 'pi pi-bolt',
+            shortcut: '⌘+S',
           },
           {
-              label: 'Features',
-              icon: 'pi pi-star'
+            label: 'Blocks',
+            icon: 'pi pi-server',
+            shortcut: '⌘+B',
           },
           {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Core',
-                      icon: 'pi pi-bolt',
-                      shortcut: '⌘+S'
-                  },
-                  {
-                      label: 'Blocks',
-                      icon: 'pi pi-server',
-                      shortcut: '⌘+B'
-                  },
-                  {
-                      label: 'UI Kit',
-                      icon: 'pi pi-pencil',
-                      shortcut: '⌘+U'
-                  },
-                  {
-                      separator: true
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette',
-                              badge: '2'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette',
-                              badge: '3'
-                          }
-                      ]
-                  }
-              ]
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+            shortcut: '⌘+U',
           },
           {
-              label: 'Contact',
-              icon: 'pi pi-envelope',
-              badge: '3'
-          }
-      ];
+            separator: true,
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+                badge: '2',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+                badge: '3',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        badge: '3',
+      },
+    ];
   }
 }
