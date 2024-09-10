4b66fe71c1282dedc15bae6327f02b3fc98263ce
Livia Soare <liv13soare@gmail.com>
2024-09-10 18:03:29 +0300

    removed unnecessary imports for angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalendarModule } from 'primeng/calendar';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WorkoutComponent } from "./modules/workout/workout/workout.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MenubarModule } from 'primeng/menubar';
 import { HeaderComponent } from './shared/header/header.component';
 import { GeneralLayoutComponent } from "./modules/general-layout/general-layout.component";
-import { GeneralLayoutModule } from './modules/general-layout/general-layout.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,14 +17,9 @@ import {MatButtonModule} from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ButtonModule,
-    CardModule,
     HeaderComponent,
-    MenubarModule,
     WorkoutComponent,
     GeneralLayoutComponent,
-    GeneralLayoutModule,
-    MatButtonModule
 ],
   providers: [
     provideAnimationsAsync()
